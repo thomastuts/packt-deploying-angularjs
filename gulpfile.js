cefc388be9743cbc5658c6dfcaf1df8a1b592b41
Thomas Tuts <thomas@darwinanalytics.com>
2014-12-14 18:21:54 +0100

    [2-1] Add Karma and JSHint for testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 'use strict';
 
 var gulp = require('gulp');
+var karma = require('karma').server;
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 
-gulp.task('hello', function () {
-  console.log('Hello from Gulp!');
+gulp.task('test', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done);
 });
+
+gulp.task('jshint', function () {
+  return gulp.src('src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+})
---
+++ test/sanity.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Sanity', function () {
+  it('should work properly', function () {
+    true.should.equal(true);
+  });
+});
