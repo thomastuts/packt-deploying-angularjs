7fd59f95041c28cfc077ca97196246f5bc480dfb
Thomas Tuts <thomas@darwinanalytics.com>
2014-12-14 20:51:20 +0100

    [2-2] Integrate BrowserSync and SASS compilation

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp');
 var karma = require('karma').server;
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
 
 gulp.task('test', function (done) {
   karma.start({
@@ -16,4 +18,31 @@ gulp.task('jshint', function () {
   return gulp.src('src/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter(stylish));
-})
+});
+
+gulp.task('serve', ['watch'], function () {
+  browserSync({
+    server: {
+      baseDir: './src'
+    }
+  });
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/styles/app.scss')
+    .pipe(sass())
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(gulp.dest('src/styles'));
+});
+
+gulp.task('reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/styles/**/*.scss', ['sass']);
+  gulp.watch('src/**/*.html', ['reload']);
+  gulp.watch('src/*/*.js', ['reload']);
+});
+
+gulp.task('default', ['serve']);
