553768936c2281fab35703782b575351a7dd88d0
Thomas Tuts <thomas@darwinanalytics.com>
2014-12-15 21:22:01 +0100

    [4-2] Calculating the animal's score

--- src/scripts/services/arena.js
+++ src/scripts/services/arena.js
@@ -5,6 +5,11 @@ angular.module('packt')
 
     var Arena = this;
 
+    var weights = {
+      ferocity: 1,
+      tenacity: 0.8
+    };
+
     Arena.generateMatchup = function () {
       var opponent = Animals.getRandomAnimal();
       var terrain = Terrains.getRandomTerrain();
@@ -13,6 +18,22 @@ angular.module('packt')
         opponent: opponent,
         terrain: terrain
       }
+    };
+
+    Arena.calculateCombatScore = function (animal) {
+      return ((animal.stats.ferocity * weights.ferocity) + (animal.stats.tenacity * weights.tenacity)) / 2;
+    };
+
+    Arena.calculateTerrainInfluence = function (animal, terrain) {
+      var landInfluence = animal.stats.land / terrain.stats.land;
+      var waterInfluence = animal.stats.water / terrain.stats.water;
+      return (landInfluence + waterInfluence) / 20;
+    };
+
+    Arena.calculateTotalScore = function (animal, terrain) {
+      var combatScore = Arena.calculateCombatScore(animal);
+      var terrainScore = Arena.calculateTerrainInfluence(animal, terrain);
+      return combatScore * terrainScore;
     };
 
   });
--- test/arena.js
+++ test/arena.js
@@ -14,4 +14,29 @@ describe('Arena', function () {
     }));
   });
 
+  describe('Scoring', function () {
+
+    beforeEach(inject(function (Animals, Terrains) {
+      this.rhino = Animals.find('rhino');
+      this.alligator = Animals.find('alligator');
+      this.swamp = Terrains.find('swamp');
+    }))
+
+    it('should calculate the combat score for an animal', inject(function (Arena) {
+      var combatScore = Arena.calculateCombatScore(this.rhino);
+      combatScore.should.be.gte(0).and.lte(10);
+    }));
+
+    it('should calculate the terrain influence of a terrain and an animal', inject(function (Arena) {
+      var terrainInfluence = Arena.calculateTerrainInfluence(this.rhino, this.swamp);
+      terrainInfluence.should.be.gte(0).and.lte(1);
+    }));
+
+    it('should calculate the final score for an animal based on its stats and the terrain', inject(function (Arena) {
+      var totalScore = Arena.calculateTotalScore(this.rhino, this.swamp);
+      totalScore.should.be.gte(0).and.lte(10);
+    }));
+
+  });
+
 });
