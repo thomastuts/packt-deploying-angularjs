08831995be1029fdf6d1670662cc94899639f532
Thomas Tuts <thomas@darwinanalytics.com>
2014-12-19 09:53:37 +0100

    [6-2] Building the production version

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,14 @@ var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
+var htmlReplace = require('gulp-html-replace');
+var concat = require('gulp-concat');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify = require('gulp-uglify');
+var es = require('event-stream');
+var ngTemplateCache = require('gulp-angular-templatecache');
+var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
 
 gulp.task('test', function (done) {
   karma.start({
@@ -28,6 +36,14 @@ gulp.task('serve', ['watch'], function () {
   });
 });
 
+gulp.task('serve:dist', function () {
+  browserSync({
+    server: {
+      baseDir: './dist'
+    }
+  });
+});
+
 gulp.task('sass', function () {
   return gulp.src('src/styles/app.scss')
     .pipe(sass())
@@ -39,10 +55,57 @@ gulp.task('reload', function () {
   browserSync.reload();
 });
 
+gulp.task('htmlreplace', function () {
+  return gulp.src('src/index.html')
+    .pipe(htmlReplace({
+      'vendor': 'scripts/vendor.js',
+      'source': 'scripts/bundle.js'
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('bundle:source', function () {
+
+  return es.merge(
+    gulp.src('src/scripts/**/*.html')
+      .pipe(ngTemplateCache({
+        module: 'packt',
+        root: 'scripts'
+      })),
+    gulp.src([
+      'src/scripts/app.js',
+      'src/scripts/**/*.js'
+    ])
+  )
+    .pipe(concat('bundle.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('bundle:vendor', function () {
+  return gulp.src([
+    'src/bower_components/angular/angular.min.js',
+    'src/bower_components/angular-local-storage/dist/angular-local-storage.min.js',
+    'src/bower_components/angular-ui-router/release/angular-ui-router.min.js'
+  ])
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('dist/scripts'));
+});
+
+gulp.task('copy:styles', ['sass'], function () {
+  gulp.src('src/styles/app.css')
+    .pipe(autoprefixer())
+    .pipe(csso())
+    .pipe(gulp.dest('dist/styles'));
+});
+
 gulp.task('watch', function () {
   gulp.watch('src/styles/**/*.scss', ['sass']);
   gulp.watch('src/**/*.html', ['reload']);
   gulp.watch('src/*/*.js', ['reload']);
 });
+
+gulp.task('build', ['htmlreplace', 'bundle:source', 'bundle:vendor', 'copy:styles']);
 
 gulp.task('default', ['serve']);
